47f1959723c7479797a1df2f449ebb1fa5f2cfbd
GuoShaoHong <guoshaohong@ehang.com>
2020-07-30 09:37:38 +0800

    添加了一些motionlayout的内容

---
+++ app/src/main/java/com/kowksiuwang/everythingaboutandroid/motionlayout/MotionTest1.kt
@@ -0,0 +1,13 @@
+package com.kowksiuwang.everythingaboutandroid.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kowksiuwang.everythingaboutandroid.R
+
+class MotionTest1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_test1)
+    }
+}
