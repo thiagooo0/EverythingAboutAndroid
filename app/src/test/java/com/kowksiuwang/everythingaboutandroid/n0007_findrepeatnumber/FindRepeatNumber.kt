8d9786c5ec91cca97690564f0912ccd07e5304ed
KowkSiuWang <576713880@qq.com>
2020-08-08 22:13:37 +0800

    补充《找出数组中重复的数字》解答

--- app/src/test/java/com/kowksiuwang/everythingaboutandroid/n0007_findrepeatnumber/FindRepeatNumber.kt
+++ app/src/test/java/com/kowksiuwang/everythingaboutandroid/n0007_findrepeatnumber/FindRepeatNumber.kt
@@ -41,22 +41,27 @@ class FindRepeatNumber {
                     7,
                     8,
                     9,
-                    12,
-                    13,
-                    14,
-                    15,
-                    16,
+                    10,
+                    9,
+                    1,
+                    8,
+                    1,
                     1,
                     6
                 )
-            ), 1
+            ), 9
         )
     }
 
-    /**
-     * 这里可以用hashset，同时因为2 <= n <= 100000，所以也可以用一个IntArray(100_001)来做判断
-     */
     fun findRepeatNumberTest(nums: IntArray): Int {
+//        return findRepeatNumberTest1(nums)
+        return findRepeatNumberTest3(nums)
+    }
+
+    /**
+     * 解法1：这里可以用hashset
+     */
+    fun findRepeatNumberTest1(nums: IntArray): Int {
         val set = HashSet<Int>()
         if (nums.isEmpty()) {
             return -1
@@ -66,6 +71,37 @@ class FindRepeatNumber {
                 return i
             } else {
                 set.add(i)
+            }
+        }
+        return -1
+    }
+
+
+    /**
+     * 解法2：因为2 <= n <= 100000，所以可以用一个IntArray(100_001)来做判断。
+     */
+    //留空
+// 1,2,3,4,5,5
+    /**
+     * 解法3：因为长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内，所以可以把数组本身拿来做哈希表。
+     * 这样算法复杂度和解法1相同为n，但是空间复杂度降低了。
+     */
+
+    fun findRepeatNumberTest3(nums: IntArray): Int {
+        if (nums.isEmpty()) {
+            return -1
+        }
+        var n: Int
+        for (i in nums.indices) {
+            while (i != nums[i]) {
+                //如果i位置上的数和目标位置上的数相同，就可以返回了
+                if (nums[i] == nums[nums[i]]) {
+                    return nums[i]
+                }
+                //否则就把nums[i]位置上的数放到index为num[i]的位置上去。
+                n = nums[i]
+                nums[i] = nums[nums[i]]
+                nums[n] = n
             }
         }
         return -1
